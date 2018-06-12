5f740dd6313e5ae394f181f29d0fb83501009508
Luthfi Azzammi <luthfia911@gmail.com>
2018-06-12 04:47:02 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,7 @@
     $MsBuildPath = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
     $MsBuildPath64 = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\amd64\\MSBuild.exe"
-    $NugetPath = "C:\\gitlab-runner\\nuget"
     $SolutionName = "Invoice OTC.sln"
   
- 
-    echo "Sedang merestore paket nuget untuk $SolutionName"
-    $NugetPath restore $SolutionName  
-   
     echo "Sedang melakukan proses build 32 bit untuk $SolutionName ..."
     $MsBuildPath $SolutionName    
     echo "Sedang melakukan proses build 64 bit untuk $SolutionName ..."
